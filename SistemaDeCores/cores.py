c8afcd6faa4987177b9f3d234eb0731a2df28a7b
Edson-Padilha <edsonp.bsi@gmail.com>
2022-02-13 21:55:01 -0300

    Subindo arquivo de cores

---
+++ SistemaDeCores/cores.py
@@ -0,0 +1,34 @@
+import kivy
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
+
+#Window.clearcolor = [1, 1, 1, 1]
+Window.clearcolor = get_color_from_hex("#ffffff")
+
+kvcode = """
+#:import C kivy.utils.get_color_from_hex
+
+<FVerde@FloatLayout>:
+    size_hint: .2, .3
+    canvas.before:
+        Color:
+            rgba: C("#22FFAA")
+        Rectangle:
+            pos: self.pos
+            size: self.size
+
+FloatLayout:
+    FVerde:
+        pos_hint:{"x":.4, "y":.4}
+"""
+
+
+
+class JanelaApp(App):
+    def build(self):
+        return Builder.load_string(kvcode)
+
+janela = JanelaApp()
+janela.run()
